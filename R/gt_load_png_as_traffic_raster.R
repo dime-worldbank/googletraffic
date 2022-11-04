e25b54ead6a9c816ee197d136341aaf318f44cc3
Rob Marty <ramarty@email.wm.edu>
2022-11-04 14:35:55 -0400

    warnings

--- R/gt_load_png_as_traffic_raster.R
+++ R/gt_load_png_as_traffic_raster.R
@@ -40,86 +40,84 @@ gt_load_png_as_traffic_raster <- function(filename,
   # Code produces some warnings that are not relevant; for example, when initially
   # make a raster, we get a warning that the extent is not defined. This warning
   # can be ignored as the extent is defined later.
-  suppressMessages({
-    suppressWarnings({
-      
-      #### Set latitude and longitude
-      latitude  <- location[1]
-      longitude <- location[2]
-      
-      #### Load
-      r   <- raster::raster(filename,1)
-      img <- png::readPNG(filename)
-      
-      #### Assign traffic colors 
-      ## Image to hex
-      rimg <- raster::as.raster(img) 
-      
-      colors_df <- rimg %>% 
-        table() %>% 
-        as.data.frame() %>%
-        dplyr::rename(hex = ".")
-      
-      colors_df$hex <- colors_df$hex %>% 
-        as.character()
-      
-      ## Assign traffic colors based on hsl
-      hsl_df <- colors_df$hex %>% 
-        plotwidgets::col2hsl() %>%
-        t() %>%
-        as.data.frame() 
-      
-      colors_df <- dplyr::bind_cols(colors_df, hsl_df)
-      
-      colors_df <- colors_df %>%
-        dplyr::mutate(color = case_when(#((H == 0) & (S < 0.2)) ~ "background",
-          ((H == 0) & (S >= 0.28) & (S < 0.7) & (L >= 0.3) & (L <= 0.42)) ~ "dark-red",
-          H > 0 & H <= 5 & L <= 0.65 ~ "red", # L <= 0.80
-          H >= 20 & H <= 28 & L <= 0.80 ~ "orange", # L <= 0.85
-          H >= 120 & H <= 135 & L <= 0.80 ~ "green"))
-      
-      ## Apply traffic colors to raster
-      colors_unique <- colors_df$color %>% unique()
-      colors_unique <- colors_unique[!is.na(colors_unique)]
-      colors_unique <- colors_unique[!(colors_unique %in% "background")]
-      rimg <- matrix(rimg) #%>% raster::t() #%>% base::t()
-      for(color_i in colors_unique){
-        rimg[rimg %in% colors_df$hex[colors_df$color %in% color_i]] <- color_i
-      }
-      
-      r[] <- NA
-      r[rimg %in% "green"]    <- 1
-      r[rimg %in% "orange"]   <- 2
-      r[rimg %in% "red"]      <- 3
-      r[rimg %in% "dark-red"] <- 4
-      
-      ## Spatially define raster
-      ext_4326 <- gt_make_extent(latitude = latitude,
-                                 longitude = longitude,
-                                 height = height,
-                                 width = width,
-                                 zoom = zoom)
-      
-      # Project extent to 3857
-      ext_3857 <- ext_4326 %>% 
-        sf::st_bbox() %>% 
-        sf::st_as_sfc() 
-      
-      sf::st_crs(ext_3857) <- 4326
-      
-      ext_3857 <- ext_3857 %>%
-        sf::st_transform(3857) %>% 
-        sf::st_bbox() %>% 
-        raster::extent()
-      
-      raster::extent(r) <- ext_3857
-      
-      raster::crs(r) <- sp::CRS("+init=epsg:3857")
-      
-      ## Convert to EPSG:4326
-      r <- raster::projectRaster(r, crs = CRS("+init=epsg:4326"), method = "ngb")
-      
-    })
+  suppressWarnings({
+    
+    #### Set latitude and longitude
+    latitude  <- location[1]
+    longitude <- location[2]
+    
+    #### Load
+    r   <- raster::raster(filename,1)
+    img <- png::readPNG(filename)
+    
+    #### Assign traffic colors 
+    ## Image to hex
+    rimg <- raster::as.raster(img) 
+    
+    colors_df <- rimg %>% 
+      table() %>% 
+      as.data.frame() %>%
+      dplyr::rename(hex = ".")
+    
+    colors_df$hex <- colors_df$hex %>% 
+      as.character()
+    
+    ## Assign traffic colors based on hsl
+    hsl_df <- colors_df$hex %>% 
+      plotwidgets::col2hsl() %>%
+      t() %>%
+      as.data.frame() 
+    
+    colors_df <- dplyr::bind_cols(colors_df, hsl_df)
+    
+    colors_df <- colors_df %>%
+      dplyr::mutate(color = case_when(#((H == 0) & (S < 0.2)) ~ "background",
+        ((H == 0) & (S >= 0.28) & (S < 0.7) & (L >= 0.3) & (L <= 0.42)) ~ "dark-red",
+        H > 0 & H <= 5 & L <= 0.65 ~ "red", # L <= 0.80
+        H >= 20 & H <= 28 & L <= 0.80 ~ "orange", # L <= 0.85
+        H >= 120 & H <= 135 & L <= 0.80 ~ "green"))
+    
+    ## Apply traffic colors to raster
+    colors_unique <- colors_df$color %>% unique()
+    colors_unique <- colors_unique[!is.na(colors_unique)]
+    colors_unique <- colors_unique[!(colors_unique %in% "background")]
+    rimg <- matrix(rimg) #%>% raster::t() #%>% base::t()
+    for(color_i in colors_unique){
+      rimg[rimg %in% colors_df$hex[colors_df$color %in% color_i]] <- color_i
+    }
+    
+    r[] <- NA
+    r[rimg %in% "green"]    <- 1
+    r[rimg %in% "orange"]   <- 2
+    r[rimg %in% "red"]      <- 3
+    r[rimg %in% "dark-red"] <- 4
+    
+    ## Spatially define raster
+    ext_4326 <- gt_make_extent(latitude = latitude,
+                               longitude = longitude,
+                               height = height,
+                               width = width,
+                               zoom = zoom)
+    
+    # Project extent to 3857
+    ext_3857 <- ext_4326 %>% 
+      sf::st_bbox() %>% 
+      sf::st_as_sfc() 
+    
+    sf::st_crs(ext_3857) <- 4326
+    
+    ext_3857 <- ext_3857 %>%
+      sf::st_transform(3857) %>% 
+      sf::st_bbox() %>% 
+      raster::extent()
+    
+    raster::extent(r) <- ext_3857
+    
+    raster::crs(r) <- sp::CRS("+init=epsg:3857")
+    
+    ## Convert to EPSG:4326
+    r <- raster::projectRaster(r, crs = CRS("+init=epsg:4326"), method = "ngb")
+    
   })
   
   return(r)
