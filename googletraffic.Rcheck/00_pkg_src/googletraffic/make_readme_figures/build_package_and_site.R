0a3e889eec2c4b9d0df80c5deb7c23433f735ebe
Rob Marty <ramarty@email.wm.edu>
2022-11-04 15:18:37 -0400

    rm

--- googletraffic.Rcheck/00_pkg_src/googletraffic/make_readme_figures/build_package_and_site.R
+++
@@ -1,9 +0,0 @@
-if(F){
-  roxygen2::roxygenise("~/Documents/Github/googletraffic")
-  
-  setwd("~/Documents/Github/googletraffic")
-  pkgdown::deploy_to_branch()
-  
-  #R CMD build --as-cran "~/Documents/Github/googletraffic"
-  #R CMD check --as-cran "~/Documents/Github/googletraffic"
-}
